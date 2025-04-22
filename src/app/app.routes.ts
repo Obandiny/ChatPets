b4de93c6f85d334d0816e39aa7b7df6fc3ef80df
Obandiny <johnoe2000@gmail.com>
2025-04-22 16:12:58 -0500

    Cambios en proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { ChatbotComponent } from './chatbot/chatbot.component';
 import { InformacionComponent } from './informacion/informacion.component';
@@ -9,7 +8,6 @@ import { ConfiguracionComponent } from './Configuracion/configuracion.component'
 export const routes: Routes = [
     { path: '', redirectTo: 'auth/register', pathMatch: 'full' },
     { path: 'chatbot', component: ChatbotComponent },
-    { path: 'auth/login', component: LoginComponent},
     { path: 'auth/register', component: RegisterComponent },
     { path: 'informacion', component: InformacionComponent },
     { path: 'diagnostico', component: ConfiguracionComponent },
