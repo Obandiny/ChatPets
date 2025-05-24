aebeceadc767b9fa4fa2f692a257935c8f85d2cd
Obandiny <johnoe2000@gmail.com>
2025-05-23 20:01:34 -0500

    Cambios 23/05/2025

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,14 @@
 import { Routes } from '@angular/router';
-import { RegisterComponent } from './auth/register/register.component';
 import { ChatbotComponent } from './chatbot/chatbot.component';
 import { InformacionComponent } from './informacion/informacion.component';
 import { ConfiguracionComponent } from './Configuracion/configuracion.component';
-import { InicioComponent } from './inicio/inicio.component';
 import { EleccionComponent } from './eleccion/eleccion.component';
 
 
 export const routes: Routes = [
     { path: '', redirectTo: 'inicio', pathMatch: 'full' },
     { path: 'chatbot', component: ChatbotComponent },
-    { path: 'auth/register', component: RegisterComponent },
     { path: 'informacion', component: InformacionComponent },
     { path: 'diagnostico', component: ConfiguracionComponent },
-    { path: 'inicio', component: InicioComponent },
     { path: 'eleccion', component: EleccionComponent }
 ];
