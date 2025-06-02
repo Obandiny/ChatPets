3e4b5db02423eea1a44bc4e7b104285e210033fd
Obandiny <johnoe@gmail.com>
2025-06-02 12:31:34 -0500

    Cambios 02/06/2025

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
