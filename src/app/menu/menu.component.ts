dc48300a930aec90c24ce3c6293a1cf897752053
Obandiny <johnoe@gmail.com>
2025-06-03 10:18:09 -0500

    Cambios 03/06/2025

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [
-    MatIconModule
+    MatIconModule,
+    RouterLink
   ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  isMenuOpen = false;
 
+  constructor() {}
+
+  openMenu() {
+    this.isMenuOpen = true;
+    console.log("Click");
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
